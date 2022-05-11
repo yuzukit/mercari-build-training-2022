e6a8baf998d1a1422f8162b09fe1a5f33ff4f3eb
Yuzuki <yuzuki.t@toyamaketsugatsunoMacBook-puro.local>
2022-05-11 18:57:45 +0900

    Finished 3-5

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import os
 import logging
 import pathlib
 import json
+import sqlite3
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,19 +26,42 @@ def root():
 
 @app.post("/items")
 def add_item(name: str = Form(...), category: str = Form(...)):
-    with open("items.json") as f:
-        df = json.load(f)
-    df["items"].append({"name": name, "category": category})
-    with open("items.json", "w") as f:
-        json.dump(df, f, indent = 4)
+    db = sqlite3.connect('../db/mercari.sqlite3')
+    cur = db.cursor()
+    sql = "INSERT INTO items (name, category) VALUES (?, ?)"
+    cur.execute(sql, (name, category,))
+    db.commit()
+    db.close()
+    # with open("items.json") as f:
+    #     df = json.load(f)
+    # df["items"].append({"name": name, "category": category})
+    # with open("items.json", "w") as f:
+    #     json.dump(df, f, indent = 4)
     logger.info(f"Receive item: {name}")
     return {"message": f"item received: {name}"}
 
 @app.get("/items")
 def get_item():
-    with open("items.json") as f:
-        df = json.load(f)
-    return df
+    db = sqlite3.connect('../db/mercari.sqlite3')
+    cur = db.cursor()
+    sql = "SELECT name, category FROM items"
+    cur.execute(sql)
+    items_list = cur.fetchall()
+    db.close()
+    return items_list
+    # with open("items.json") as f:
+    #     df = json.load(f)
+    # return df
+
+@app.get("/search")
+def search_item(keyword: str):
+    db = sqlite3.connect('../db/mercari.sqlite3')
+    cur = db.cursor()
+    sql = "SELECT name, category FROM items WHERE name=?"
+    cur.execute(sql, (keyword,))
+    docs = cur.fetchall()
+    db.close()
+    return docs
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
